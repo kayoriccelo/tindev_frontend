dcd7397024ea137fcfe575ce62f67f9e540417da
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-09-02 12:22:38 -0300

    Finalizando Omnistack;

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
-import './Main.css';
+import io from 'socket.io-client';
 
+import './Main.css';
 import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -17,20 +20,30 @@ export default function Main({ match }) {
                 headers: {
                     user: match.params.id
                 }
-            })
+            });
 
             setUsers(response.data);
-        }
+        };
 
         loadUsers();
-    }, [match.params.id])
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
+    }, [match.params.id]);
 
     async function handleDislike(id) {
         await api.post(`/devs/${id}/dislikes`, null, {
             headers: {
                 user: match.params.id
             }
-        })
+        });
 
         setUsers(users.filter(user => user._id !== id));
     }
@@ -50,9 +63,9 @@ export default function Main({ match }) {
             <Link to='/'>
                 <img src={logo} alt="Tindev" />
             </Link>
-            <ul>
-                {users.length > 0 ?
-                    users.map(user =>
+            {users.length > 0 ? (
+                <ul>
+                    {users.map(user =>
                         (<li key={user._id}>
                             <img src={user.avatar} alt={user.name} />
                             <footer>
@@ -68,10 +81,22 @@ export default function Main({ match }) {
                                 </button>
                             </div>
                         </li>)
-                    ) : (
-                        <div className="empty">Acabou :(</div>
                     )}
-            </ul>
+                </ul>
+            ) : (
+                    <div className="empty">Acabou :(</div>
+                )}
+
+            {matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match"/>
+                    <img className="avatar" src={matchDev.avatar} alt="It's a match"/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button type="button" onClick={() => setMatchDev(null)}>Fechar</button>
+                </div> 
+            )}
+
         </div>
     )
-}
+} 
